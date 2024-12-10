781ce0ce48dbf47edb703b1ecb51920047b3ec8c
NefziM <nefzimalek2002@gmail.com>
2024-12-10 19:03:38 +0100

    version 4

--- src/test/java/Steps/TestRunner.java
+++ src/test/java/Steps/TestRunner.java
@@ -6,7 +6,9 @@ import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features="src/test/resources/Features", glue= {"Steps"},monochrome= true
-, plugin = {"json:target/Reports/report.json"},
+, plugin = {"pretty" , "junit:target/Reports/report.xml",
+		"json:target/Reports/report.json",
+		"html:target/Reports/report.html"},
 tags="@dispolivre")
 public class TestRunner {
 	
